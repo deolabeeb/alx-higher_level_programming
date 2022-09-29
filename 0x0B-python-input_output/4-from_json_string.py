da22c4e38013375533b3fcd1c444d02eec15a41c
Abeeb Adesina <deolabeeb@gmail.com>
2022-09-29 04:23:31 +0000

    JSON string

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
+"""
+Contains the "from_json_string" function
+"""
+
 import json
-"""
-This file contains a function that
-returns an object represented by a
-JSON string
-"""
 
 
 def from_json_string(my_str):
-    """
-    function to return object
-    represented by a JSON string
-    """
-    return (json.loads(my_str))
+    """returns an object represented by a JSON string"""
+    return json.loads(my_str)
